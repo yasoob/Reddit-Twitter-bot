31d64412d57385b6afd05fd4c22b20c325568c8c
Zachary Kagan <zakagan@gmail.com>
2016-03-17 01:31:30 -0500

    Updated this bot so that it correctly interacts with google's developer api authentication, and corrects other issues

--- reddit_bot.py
+++ reddit_bot.py
@@ -9,27 +9,38 @@ access_token_secret = 'YOUR ACCESS TOKEN SECRET'
 consumer_key = 'YOUR CONSUMER KEY'
 consumer_secret = 'YOUR CONSUMER SECRET'
 
-def strip_title(title):
-	if len(title) < 94:
+google_api_key = "YOUR GOOGLE API KEY"  
+
+subreddit_name = "subredditsimulator"
+tag_string= "#Python #reddit #bot"   #Place to put your hastags, etc. Must be <=116 char
+num_tweets_before_stopping=20
+tweet_delay= 10						#in minutes
+
+def strip_title(title, tag_len):
+	char_remaining=140-tag_len-26		# 26 = 24 for link + 2 for the spaces between concatenated strings
+	if len(title) <= char_remaining:
 		return title
+	elif char_remaining >= 3:
+		return title[:char_remaining-3] + "..."
 	else:
-		return title[:93] + "..."
+		return ""	
 
 def tweet_creator(subreddit_info):
-	post_dict = {}
+	post_titles = []
+	post_links = []
 	post_ids = []
 	print "[bot] Getting posts from Reddit"
 	for submission in subreddit_info.get_hot(limit=20):
-		post_dict[strip_title(submission.title)] = submission.url
-		post_ids.append(submission.id)
-	print "[bot] Generating short link using goo.gl"
-	mini_post_dict = {}
-	for post in post_dict:
-		post_title = post
-		post_link = post_dict[post]   		
-		short_link = shorten(post_link)
-		mini_post_dict[post_title] = short_link 
-	return mini_post_dict, post_ids
+		post_id=submission.id
+		post_link=submission.url
+		post_title=strip_title(submission.title, len(tag_string))
+
+		post_titles.append(post_title)
+		post_links.append(post_link)
+		post_ids.append(post_id)
+
+		del post_title, post_link, post_id
+	return post_titles, post_links, post_ids
 
 def setup_connection_reddit(subreddit):
 	print "[bot] setting up connection with Reddit"
@@ -38,13 +49,17 @@ def setup_connection_reddit(subreddit):
 	subreddit = r.get_subreddit(subreddit)
 	return subreddit
 
-def shorten(url):
-	headers = {'content-type': 'application/json'}
-	payload = {"longUrl": url}
-	url = "https://www.googleapis.com/urlshortener/v1/url"
-	r = requests.post(url, data=json.dumps(payload), headers=headers, verify=False)
-	link = json.loads(r.text)['id']
-	return link
+def shorten(url):		# Adjusted to include google api key authentication
+	try:
+		headers = {'content-type': 'application/json'}
+		payload = {"longUrl": url}
+		googl_url = "https://www.googleapis.com/urlshortener/v1/url?key=%s" %(config_dict['google_api_key'])
+		r = requests.post(googl_url, data=json.dumps(payload), headers=headers)
+		url = json.loads(r.text)['id']
+		print "[bot] Generating short link using goo.gl"
+	except:
+		print "[bot] improper google api key, defaulting to twitter's t.co shortner"
+	return url
 
 def duplicate_check(id):
 	found = 0
@@ -52,31 +67,52 @@ def duplicate_check(id):
 		for line in file:
 			if id in line:
 				found = 1
+	file.close()			
 	return found
 
 def add_id_to_file(id):
 	with open('posted_posts.txt', 'a') as file:
 		file.write(str(id) + "\n")
+	file.close()		
 
 def main():
-	subreddit = setup_connection_reddit('python')
-	post_dict, post_ids = tweet_creator(subreddit)
-	tweeter(post_dict, post_ids)
+	count=0
+	if len(tag_string) > 116:
+		print "[bot] Trailing string of tags is too long, please limit to <100 char"
+		return
+	while count <= num_tweets_before_stopping:
+		subreddit = setup_connection_reddit(subreddit_name)
+		post_titles, post_links, post_ids = tweet_creator(subreddit)
+		tweeter(post_titles, post_links, post_ids)
+		print "[bot] waiting until next tweet"		
+		time.sleep(tweet_delay*60)
+		count+=1
 
-def tweeter(post_dict, post_ids):
+def tweeter(post_titles, post_links, post_ids):
 	auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
 	auth.set_access_token(access_token, access_token_secret)
 	api = tweepy.API(auth)
-	for post, post_id in zip(post_dict, post_ids):
+	index=0
+	for post_title, post_link, post_id in zip(post_titles, post_links, post_ids):
 		found = duplicate_check(post_id)
 		if found == 0:
+			tweet_content=post_title+" "+shorten(post_link)+" "+tag_string
 			print "[bot] Posting this link on twitter"
-			print post+" "+post_dict[post]+" #Python #reddit #bot"
-			api.update_status(post+" "+post_dict[post]+" #Python #reddit #bot")
+			print tweet_content.encode("utf-8")
+			try:
+				api.update_status(tweet_content)
+			except Exception, e:	
+				print  "[bot] Error triggered when sending tweet content to twitter:"
+				try:
+					print "[Twitter] "+ e.args[0][0]['message']
+				except:
+					print "[bot] Error outside of communication with Twitter"	
+				return	
 			add_id_to_file(post_id)
-			time.sleep(30)
+			return
 		else:
-			print "[bot] Already posted" 
+			print "[bot] ID for post #%d already collected" %(index)
+			index+=1
 
 if __name__ == '__main__':
 	main()
